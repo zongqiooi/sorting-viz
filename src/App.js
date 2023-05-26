675b126f4ba2de8797f11aed0fb1228c180dc14d
Zong Qi <huro710@hotmail.com>
2023-05-26 16:14:15 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from "./logo.svg";
+import SortingVisualizer from "./SortingVisualizer/Components/SortingVisualizer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualizer></SortingVisualizer>
+    </div>
+  );
+}
+
+export default App;
