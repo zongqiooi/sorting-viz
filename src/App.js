aa0cb0efef3f692d0f6e00c11c9e590bc2ab604b
Zong Qi <huro710@hotmail.com>
2023-05-29 21:25:37 +0800

    Added toaster, switcher, and footer components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-// import logo from "./logo.svg";
-import SortingVisualizer from "./SortingVisualizer/Components/SortingVisualizer";
-import "./App.css";
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import Switcher from "./utils/Switcher";
+import SortingVisualizer from "./components/SortingVisualizer";
+import Footer from "./components/Footer";
+import "./css/SortingVisualizer.css";
 
 function App() {
   return (
-    <div className="App">
-      <SortingVisualizer></SortingVisualizer>
+    <div class="bg-white dark:bg-gray-800 w-screen h-screen overflow-hidden">
+      <Switcher />
+      <Toaster />
+      <SortingVisualizer />
+      <Footer />
     </div>
   );
 }
